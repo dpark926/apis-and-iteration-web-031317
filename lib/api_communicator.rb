9ef421f41abe855c2d2e77e19ac95be24bb13a1a
Rob Weber <rob.weber87@gmail.com>
2017-03-15 18:21:06 -0400

    semi complete star wars api project

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,12 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+
+  character_hash["results"].each do |character_array|
+    if character_array["name"].downcase == character.downcase 
+      return character_array["films"]
+    end
+  end
   
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
@@ -18,8 +24,17 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+
+
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  idx = 1
+  films_hash.each do |film|
+    all_films = RestClient.get(film)
+    film_hash = JSON.parse(all_films)
+    puts "#{idx}. #{film_hash["title"]}"
+    idx += 1
+  end
 end
 
 def show_character_movies(character)
@@ -31,3 +46,6 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,31 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
 def welcome
+	puts "Star Wars API welcome to, Youngling."
   # puts out a welcome message here!
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character:"
+  user_input = gets.chomp.downcase
+
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+  array = []
+
+  character_hash["results"].each do |character_array|
+    array << character_array["name"].downcase
+  end
+
+  if array.include?(user_input) == false
+  	get_character_from_user
+  else
+  	return user_input
+  end
+ 
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
